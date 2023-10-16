47b712ea0ae534cae0f248376e01ca8365062e1f
kobasisauri <kobasisauri@gmail.com>
2023-10-16 22:59:19 +0400

    add inputs in auth

---
+++ src/components/shared/customInput/Input.jsx
@@ -0,0 +1,57 @@
+import { useRef } from "react";
+import classes from "./Input.module.scss";
+
+const TextInput = ({
+  label,
+  name,
+  value,
+  type,
+  className,
+  isInvalid,
+  errorMSG,
+  handleChange,
+  handleBlur,
+  handleClick,
+  disabled,
+  placeholder,
+  prefix,
+  suffix,
+  autoComplete,
+  readOnly,
+  ...rest
+}) => {
+  const inputEl = useRef();
+  const onFocus = () => inputEl.current.focus();
+
+  return (
+    <div className={`w-100 ${className ? className : ""}`} {...rest}>
+      {label && <label className="mb-2  ms-1">{label}</label>}
+      <div
+        className={`d-flex align-items-center ${classes.wrapper} ${
+          isInvalid ? classes.warning : ""
+        } `}
+        onClick={onFocus}
+      >
+        {prefix && <div className={classes.prefix}>{prefix}</div>}
+        <input
+          ref={inputEl}
+          className={`w-100 border-0 ${disabled ? classes.disabled : ""}`}
+          type={type || "text"}
+          name={name}
+          placeholder={placeholder}
+          value={value ?? ""}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          onClick={handleClick}
+          disabled={disabled || false}
+          readOnly={readOnly}
+          autoComplete={autoComplete || "off"}
+        />
+        {suffix && <div className={classes.suffix}>{suffix}</div>}
+      </div>
+      {isInvalid && <div className={classes.errorMSG}>შეცდომა</div>}
+    </div>
+  );
+};
+
+export default TextInput;
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,29 +2,182 @@ import { useState } from "react";
 import styles from "./Login.module.scss";
 import icon from "../../assets/images/icon.png";
 import Progressbar from "../../components/shared/loadingBar/LoadingBar";
+import TextInput from "../../components/shared/customInput/Input";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
+const initialValues = {
+  email: null,
+  password: null,
+};
 const Login = () => {
   const [loading, setLoading] = useState(true);
+  const [active, setActive] = useState(0);
+  const [privateAccData, setPrivateAccData] = useState({
+    email: null,
+    password: null,
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  const {
+    values,
+    errors,
+    handleChange,
+    handleSubmit,
+    setFieldValue,
+    setValues,
+  } = useFormik({
+    initialValues,
+    validationSchema: Yup.object().shape({}),
+    onSubmit,
+  });
 
   return loading ? (
     <Progressbar setLoading={setLoading} />
   ) : (
     <div className={styles.containter}>
       <div className={styles.wrapper}>
-        <div className={styles["logo-container"]}>
+        <div className={styles["logo-container"]} onClick={() => setActive(0)}>
           <h1 className={styles.header}>CERTIFY</h1>
           <img src={icon} alt="icon" width={50} height={50} />
         </div>
 
-        <div className={styles.box}>
-          <div className={styles.button}>
-            <p className={styles.text}>Buissines Account</p>
+        {active === 0 && (
+          <div className={styles.box}>
+            <div className={styles.button} onClick={() => setActive(1)}>
+              <p className={styles.text}>Buissines Account</p>
+            </div>
+            <p>or</p>
+            <div className={styles.button} onClick={() => setActive(2)}>
+              <p className={styles.text}>Private Account</p>
+            </div>
           </div>
-          <p>or</p>
-          <div className={styles.button}>
-            <p className={styles.text}>Private Account</p>
+        )}
+        {active === 1 && (
+          <div className={styles.box2}>
+            <div style={{ color: "#616161", fontWeight: 700 }}>
+              CERTIFY WITH CERTIFY
+            </div>
+
+            <TextInput
+              placeholder={"Email"}
+              value={values.email}
+              name="email"
+              handleChange={handleChange}
+            />
+            <TextInput
+              placeholder={"Password"}
+              value={values.password}
+              name="password"
+              handleChange={handleChange}
+            />
+            <div style={{ color: "#616161", textDecoration: "underline" }}>
+              Forgot password?
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <div className={styles["sign-in-button"]}>
+                <p
+                  style={{
+                    color: "#FFF",
+                    fontSize: "20px",
+                    fontWeight: "700",
+                  }}
+                >
+                  Sign in
+                </p>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <p style={{ color: "#A3A3A3", fontSize: "15px" }}>
+                  New here?
+                  <span> Create an account</span>
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
+        )}
+
+        {active === 2 && (
+          <div className={styles.box2}>
+            <div style={{ color: "#616161", fontWeight: 700 }}>
+              CERTIFY WITH CERTIFY
+            </div>
+
+            <TextInput
+              placeholder={"Email"}
+              value={privateAccData.email}
+              handleChange={(e) =>
+                setPrivateAccData((state) => ({
+                  ...state,
+                  email: e.target.value,
+                }))
+              }
+            />
+            <TextInput
+              placeholder={"Password"}
+              value={privateAccData.password}
+              handleChange={(e) =>
+                setPrivateAccData((state) => ({
+                  ...state,
+                  password: e.target.value,
+                }))
+              }
+            />
+            <div style={{ color: "#616161", textDecoration: "underline" }}>
+              Forgot password?
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <div className={styles["sign-in-button"]}>
+                <p
+                  style={{
+                    color: "#FFF",
+                    fontSize: "20px",
+                    fontWeight: "700",
+                  }}
+                >
+                  Sign in
+                </p>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <p style={{ color: "#A3A3A3", fontSize: "15px" }}>
+                  New here?
+                  <span> Create an account</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
