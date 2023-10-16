7a77a4824de021c8b8e46cc12589e3af5f03455e
kobasisauri <kobasisauri@gmail.com>
2023-10-16 16:44:40 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { Route, Navigate, Routes, useLocation } from "react-router-dom";
+
+import { Home, Error } from "./pages";
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  // to do
+  // useEffect(() => {
+  //   setTheme(JSON.parse(localStorage.getItesm("theme")));
+  // }, [setTheme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Navigate replace="true" to="/home" />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/404" element={<Error />} />
+        <Route path="/*" element={<Navigate replace to="/404" />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+import Home from "./home/Home";
+import Error from "./error/Error";
+
+export { Home, Error };
